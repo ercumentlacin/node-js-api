010df65cd9c813ab8bb4a254a44940f3b6582072
Ercüment Laçın <ercument.lacin@akinon.com>
2021-07-03 07:47:32 +0300

    [dev] - create server with express & add data

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-const http = require('http');
+const express = require('express');
+const data = require('./data');
 
-const host = '127.0.0.1';
-const port = 3000;
+const server = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+server.get('/', (req, res) => {
+  res.send('Hello Express!');
 });
 
-server.listen(port, host, () => {
-  console.log(
-    `http://${host}:${port} adresinden gelen istekler dinleniyor ...`
-  );
+server.get('/users', (req, res) => {
+  res.status(200).json(data);
+});
+
+server.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const user = data.find((user) => user.id === parseInt(id));
+
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    res.status(404).send('Aradığınız kullanıcı bulunamadı ...');
+  }
+});
+
+server.listen(5000, () => {
+  console.log(`http://localhost:5000 adresinden gelen istekler dinleniyor ...`);
 });
