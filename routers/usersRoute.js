3da37777263dae1e1d434b26614a141f4e4fe19a
Ercüment Laçın <ercument.lacin@akinon.com>
2021-07-03 09:27:08 +0300

    [dev] - added routing feature

---
+++ routers/usersRoute.js
@@ -0,0 +1,64 @@
+const router = require('express').Router();
+let data = require('../data');
+
+router.get('/', (req, res) => {
+  res.status(200).json(data);
+});
+
+// yeni kullanıcı oluşturma
+let next_id = 4;
+router.post('/', (req, res) => {
+  let newUser = req.body;
+  newUser.id = next_id;
+  next_id++;
+  data.push(newUser);
+  res.status(201).json(newUser);
+});
+
+// kullanıcı silme
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  const deleteUser = data.find((user) => user.id === parseInt(id));
+  data = data.filter((user) => user.id !== parseInt(id));
+
+  if (deleteUser) {
+    res.send(data);
+    res.status(204).end();
+  } else {
+    res
+      .status(404)
+      .json({ errorMessage: 'Silmeye çalıştığınız kullanıcı sistemde yok !' });
+  }
+});
+
+// kullanıcı edit
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  let thisUser = data.find((user) => user.id === parseInt(id));
+
+  if (thisUser) {
+    data = data.filter((user) => user.id !== parseInt(id));
+    thisUser = { ...thisUser, ...body };
+    data = [...data, thisUser];
+    res.send(data);
+    res.status(200).end();
+  } else {
+    res
+      .status(404)
+      .json({ errorMessage: 'Silmeye çalıştığınız kullanıcı sistemde yok !' });
+  }
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const user = data.find((user) => user.id === parseInt(id));
+
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    res.status(404).send('Aradığınız kullanıcı bulunamadı ...');
+  }
+});
+
+module.exports = router;
